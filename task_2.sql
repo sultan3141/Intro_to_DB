1222b21f510096bc4113f836f7599beb55ac8d70
Sultan Adinan <Adinanyous1@gmail.com>
2025-08-17 16:51:04 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Books(
-  book_id INTEGER PRIMARY KEY,
+  book_id INT PRIMARY KEY,
   title VARCHAR(130),
   author_id FOREIGN KEY REFERENCING Authors TABLE,
   price DOUBLE,
@@ -7,22 +7,22 @@ CREATE TABLE IF NOT EXISTS Books(
   );
   
 CREATE TABLE IF NOT EXISTS Authors(
-  author_id PRIMARY KEY,
+  author_id INT PRIMARY KEY,
   author_name VARCHAR(215)
 );
 CREATE TABLE IF NOT EXISTS Customers(
- customer_id PRIMARY KEY,
+ customer_id INT PRIMARY KEY,
  customer_name VARCHAR(215) NOT NULL,
  email VARCHAR(215),
  address TEXT
 );
 CREATE TABLE IF NOT EXISTS Orders(
- order_id PRIMARY KEY,
+ order_id INT PRIMARY KEY,
  customer_id FOREIGN KEY REFERENCING Customers TABLE,
  order_date DATE
 );
 CREATE TABLE IF NOT EXISTS Order_Details(
- orderdetailid PRIMARY KEY,
+ orderdetailid INT PRIMARY KEY,
  order_id FOREIGN KEY REFERENCING Orders TABLE,
  book_id FOREIGN KEY REFERENCING Books TABLE,
  quantity DOUBLE
