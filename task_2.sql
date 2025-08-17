dd3a8c2f4e6f375127e22fcac72a82097bae6984
Sultan Adinan <Adinanyous1@gmail.com>
2025-08-17 17:17:47 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -19,11 +19,11 @@ CREATE TABLE IF NOT EXISTS Customers(
  email VARCHAR(215),
  address TEXT
 );
-CREATE TABLE IF NOT EXISTS Orders(
- order_id INT PRIMARY KEY,
- customer_id INT,
- order_date DATE
- FOREIGN KEY customer_id REFERENCES Customers(customer_id),
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 CREATE TABLE IF NOT EXISTS Order_Details(
  orderdetailid INT PRIMARY KEY,
