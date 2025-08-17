547bdb84af908220aed39112d1be57eb0ec1ffae
Sultan Adinan <Adinanyous1@gmail.com>
2025-08-17 18:10:05 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSERT INTO Customer(customer_id, customer_name, email, address)
+INSERT INTO customer(customer_id, customer_name, email, address)
 VALUE(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave')
